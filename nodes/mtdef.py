55a9e66cb905d5ae5003d493aa050b9b2676883a
Francis Colas <francis.colas@inria.fr>
2016-05-24 19:11:54 +0200

    update MIDs

--- nodes/mtdef.py
+++ nodes/mtdef.py
@@ -28,9 +28,6 @@ class MID:
     ReqDID = 0x00
     # DeviceID, 4 bytes: HH HL LH LL
     DeviceID = 0x01
-    # Compatibility for XBus Master users
-    InitMT = 0x02
-    InitMTResults = 0x03
     # Request product code in plain text
     ReqProductCode = 0x1C
     # Product code (max 20 bytes data)
@@ -39,48 +36,44 @@ class MID:
     ReqFWRev = 0x12
     # Firmware revision, 3 bytes: major minor rev
     FirmwareRev = 0x13
-    # Request data length according to current configuration
-    ReqDataLength = 0x0A
-    # Data Length, 2 bytes
-    DataLength = 0x0B
-    # Request GPS status (MTi-G only)
-    ReqGPSStatus = 0xA6
-    # GPS status (MTi-G only)
-    GPSStatus = 0xA7
 
     # Device specific messages
-    # Baudrate, 1 byte
-    SetBaudrate = 0x18
-    # Error mode, 2 bytes, 0000, 0001, 0002, 0003 (default 0001)
-    SetErrorMode = 0xDA
-    # Location ID, 2 bytes, arbitrary, default is 0
-    SetLocationID = 0x84
     # Restore factory defaults
     RestoreFactoryDef = 0x0E
+    # Baudrate, 1 byte
+    SetBaudrate = 0x18
+    # Run the built-in self test (MTi-1/10/100 series)
+    RunSelftest = 0x24
+    # Self test results, 2 bytes
+    SelftestAck = 0x25
+    # Error mode, 2 bytes, 0000, 0001, 0002, 0003 (default 0001)
+    SetErrorMode = 0xDA
     # Transmit delay (RS485), 2 bytes, number of clock ticks (1/29.4912 MHz)
     SetTransmitDelay = 0xDC
+    # Set state of OptionFlags (MTi-1/2/3), 4 + 4 bytes
+    SetOptionFlags = 0x48
+    # Location ID, 2 bytes, arbitrary, default is 0
+    SetLocationID = 0x84
 
     # Synchronization messages
-    # Synchronization settings (MTi-10/100 series only), N*12 bytes
+    # Synchronization settings (MTi-1/10/100 series only), N*12 bytes
     SetSyncSettings = 0x2C
-    # SyncIn setting (MTi only), (1+) 2 or 4 bytes depending on request
-    SetSyncInSettings = 0xD6
-    # SyncOut setting (MTi/MTi-G only), (1+) 2 or 4 bytes depending on request
-    SetSyncOutSettings = 0xD8
 
     # Configuration messages
     # Request configuration
     ReqConfiguration = 0x0C
     # Configuration, 118 bytes
     Configuration = 0x0D
-    # Output configuration (MTi-10/100 series only), N*4 bytes
-    SetOutputConfiguration = 0xC0
     # Sampling period (MTi/MTi-G only), 2 bytes
     SetPeriod = 0x04
-    # Skip factor (MTi/MTi-G only), 2 bytes
-    SetOutputSkipFactor = 0xD4
-    # Object alignment matrix, 9*4 bytes
-    SetObjectAlignment = 0xE0
+    # Extended output mode (MTi-10/100), 2 bytes, bit 4 for extended UART
+    SetExtOutputMode = 0x86
+    # Output configuration (MTi-1/10/100 series only), N*4 bytes
+    SetOutputConfiguration = 0xC0
+    # Configure NMEA data output (MTi-10/100), 2 bytes
+    SetStringOutputTypes = 0x8E
+    # Set sensor of local alignment quaternion
+    SetAlignmentRotation = 0xEC
     # Output mode (MTi/MTi-G only), 2 bytes
     SetOutputMode = 0xD0
     # Output settings (MTi/MTi-G only), 4 bytes
@@ -95,12 +88,12 @@ class MID:
     MTData2 = 0x36
 
     # XKF Filter messages
-    # Heading (MTi only), 4 bytes
-    SetHeading = 0x82
     # Reset orientation, 2 bytes
     ResetOrientation = 0xA4
-    # Request UTC time from sensor (MTI-G and MTi-10/100 series)
-    ReqUTCTime = 0x60
+    # Request or set UTC time from sensor (MTI-G and MTi-10/100 series)
+    SetUTCTime = 0x60
+    # Set correction ticks to UTC time
+    AdjustUTCTime = 0xA8
     # UTC Time (MTI-G and MTi-10/100 series), 12 bytes
     UTCTime = 0x61
     # Request the available XKF scenarios on the device
@@ -111,18 +104,51 @@ class MID:
     SetCurrentScenario = 0x64
     # Magnitude of the gravity used for the sensor fusion mechanism, 4 bytes
     SetGravityMagnitude = 0x66
+    # Latitude, Longitude and Altitude for local declination and gravity
+    # (MTi-10/100 series only), 24 bytes
+    SetLatLonAlt = 0x6E
+    # Initiate No Rotation procedure (not on MTi-G), 2 bytes
+    SetNoRotation = 0x22
+
+
+class DeprecatedMID:
+    """Deprecated message Ids."""
+    # Informational messages
+    # Compatibility for XBus Master users
+    InitMT = 0x02
+    InitMTResults = 0x03
+    # Request data length according to current configuration
+    ReqDataLength = 0x0A
+    # Data Length, 2 bytes
+    DataLength = 0x0B
+    # Request GPS status (MTi-G only)
+    ReqGPSStatus = 0xA6
+    # GPS status (MTi-G only)
+    GPSStatus = 0xA7
+
+    # Synchronization messages
+    # SyncIn setting (MTi only), (1+) 2 or 4 bytes depending on request
+    SetSyncInSettings = 0xD6
+    # SyncOut setting (MTi/MTi-G only), (1+) 2 or 4 bytes depending on request
+    SetSyncOutSettings = 0xD8
+
+    # Configuration messages
+    # Skip factor (MTi/MTi-G only), 2 bytes
+    SetOutputSkipFactor = 0xD4
+    # Object alignment matrix, 9*4 bytes
+    SetObjectAlignment = 0xE0
+
+    # XKF Filter messages
+    # Heading (MTi only), 4 bytes
+    SetHeading = 0x82
     # Lever arm of the GPSin sensor coordinates (MTi-G and MTi-700 only),
     # 3*4 bytes
     SetLeverArmGPS = 0x68
     # Magnetic declination (MTi-G only), 4 bytes
     SetMagneticDeclination = 0x6A
     # Latitude, Longitude and Altitude for local declination and gravity
-    # (MTi-10/100 series only), 24 bytes
-    SetLatLonAlt = 0x6E
     # Processing flags (not on firmware 2.2 or lower for MTi/MTi-g), 1 byte
     SetProcessingFlags = 0x20
-    # Initiate No Rotation procedure (not on MTi-G), 2 bytes
-    SetNoRotation = 0x22
 
 
 def getName(cls, value):

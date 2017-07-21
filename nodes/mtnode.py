d7544f6058b05b203f4edd3511c66d7d3b5da357
Francis Colas <francis.colas@inria.fr>
2017-07-21 14:27:23 +0200

    support delta q

--- nodes/mtnode.py
+++ nodes/mtnode.py
@@ -15,7 +15,7 @@ import time
 import datetime
 
 # transform Euler angles or matrix into quaternions
-from math import radians, sqrt
+from math import radians, sqrt, atan2
 from tf.transformations import quaternion_from_matrix, quaternion_from_euler,\
     identity_matrix
 
@@ -97,6 +97,8 @@ class XSensDriver(object):
 
         # publish a string version of all data; to be parsed by clients
         self.str_pub = rospy.Publisher('imu_data_str', String, queue_size=10)
+        self.last_delta_q_time = None
+        self.delta_q_rate = None
 
     def reset_vars(self):
         self.imu_msg = Imu()
@@ -531,6 +533,52 @@ class XSensDriver(object):
             '''Fill messages with information from 'Angular Velocity' MTData2
             block.'''
             try:
+                dqw, dqx, dqy, dqz = convert_quat(
+                    (o['Delta q0'], o['Delta q1'], o['Delta q2'],
+                     o['Delta q3']),
+                    o['frame'])
+                now = rospy.Time.now()
+                if self.last_delta_q_time is None:
+                    self.last_delta_q_time = now
+                else:
+                    # update rate (filtering needed to account for lag variance)
+                    delta_t = (now - self.last_delta_q_time).to_sec()
+                    if self.delta_q_rate is None:
+                        self.delta_q_rate = 1./delta_t
+                    delta_t_filtered = .95/self.delta_q_rate + .05*delta_t
+                    # rate in necessarily integer
+                    self.delta_q_rate = round(1./delta_t_filtered)
+                    print(delta_t, delta_t_filtered, self.delta_q_rate)
+                    self.last_delta_q_time = now
+                    # relationship between \Delta q and velocity \bm{\omega}:
+                    # \bm{w} = \Delta t . \bm{\omega}
+                    # \theta = |\bm{w}|
+                    # \Delta q = [cos{\theta/2}, sin{\theta/2)/\theta . \bm{\omega}
+                    # extract rotation angle over delta_t
+                    ca_2, sa_2 = dqw, sqrt(dqx**2 + dqy**2 + dqz**2)
+                    ca = ca_2**2 - sa_2**2
+                    sa = 2*ca_2*sa_2
+                    rotation_angle = atan2(sa, ca)
+                    # compute rotation velocity
+                    rotation_speed = rotation_angle * self.delta_q_rate
+                    f = rotation_speed / sa_2
+                    x, y, z = f*dqx, f*dqy, f*dqz
+                    self.imu_msg.angular_velocity.x = x
+                    self.imu_msg.angular_velocity.y = y
+                    self.imu_msg.angular_velocity.z = z
+                    self.imu_msg.angular_velocity_covariance = (
+                        radians(0.025), 0., 0.,
+                        0., radians(0.025), 0.,
+                        0., 0., radians(0.025))
+                    self.pub_imu = True
+                    self.vel_msg.twist.angular.x = x
+                    self.vel_msg.twist.angular.y = y
+                    self.vel_msg.twist.angular.z = z
+                    self.pub_vel = True
+                    print(x, y, z)
+            except KeyError:
+                pass
+            try:
                 x, y, z = convert_coords(o['gyrX'], o['gyrY'], o['gyrZ'],
                                          o['frame'])
                 self.imu_msg.angular_velocity.x = x
@@ -545,9 +593,10 @@ class XSensDriver(object):
                 self.vel_msg.twist.angular.y = y
                 self.vel_msg.twist.angular.z = z
                 self.pub_vel = True
+                print(x, y, z)
+                print
             except KeyError:
                 pass
-            # TODO decide what to do with 'Delta q'
 
         def fill_from_GPS(o):
             '''Fill messages with information from 'GPS' MTData2 block.'''
